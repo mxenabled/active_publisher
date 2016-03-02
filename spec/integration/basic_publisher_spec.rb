c354df8b5e88b1a1873847c930a060e5136326e3
Andrew Lewin <andrew.lewin@mx.com>
2016-03-02 10:25:00 -0700

    add support for mri in integration test

--- spec/integration/basic_publisher_spec.rb
+++ spec/integration/basic_publisher_spec.rb
@@ -16,7 +16,14 @@ def initialize_subscriber(exchange_name, route)
   connection = ::ActivePublisher::Connection.connection
   channel = connection.create_channel
   exchange = channel.topic(exchange_name)
-  channel.queue("").bind(exchange, :routing_key => route).subscribe do |metadata, payload|
-    @message = [metadata.routing_key, payload]
+
+  if ::RUBY_PLATFORM == "java"
+    channel.queue("").bind(exchange, :routing_key => route).subscribe do |metadata, payload|
+      @message = [metadata.routing_key, payload]
+    end
+  else
+    channel.queue("").bind(exchange, :routing_key => route).subscribe do |delivery_info, _, payload|
+      @message = [delivery_info.routing_key, payload]
+    end
   end
 end
