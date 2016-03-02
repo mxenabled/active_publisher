175d18b56a74727091d2ff7ebb850a0e332dc310
Andrew Lewin <andrew.lewin@mx.com>
2016-03-02 10:51:10 -0700

    use global variable to ensure the message is correct

--- spec/integration/basic_publisher_spec.rb
+++ spec/integration/basic_publisher_spec.rb
@@ -8,7 +8,7 @@ describe "a basic publisher", :integration => true do
   it "publishes messages to rabbit" do
     initialize_subscriber(exchange_name, route)
     ::ActivePublisher.publish(route, payload, exchange_name)
-    expect(@message).to eq([route, payload])
+    expect($message).to eq([route, payload])
   end
 end
 
@@ -19,11 +19,11 @@ def initialize_subscriber(exchange_name, route)
 
   if ::RUBY_PLATFORM == "java"
     channel.queue("").bind(exchange, :routing_key => route).subscribe do |metadata, payload|
-      @message = [metadata.routing_key, payload]
+      $message = [metadata.routing_key, payload]
     end
   else
     channel.queue("").bind(exchange, :routing_key => route).subscribe do |delivery_info, _, payload|
-      @message = [delivery_info.routing_key, payload]
+      $message = [delivery_info.routing_key, payload]
     end
   end
 end
