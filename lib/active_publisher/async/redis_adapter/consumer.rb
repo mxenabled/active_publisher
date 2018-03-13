7d3b623aa41f0652bef9a7419b88f937e8745d96
Garrett Thornburg <film42@gmail.com>
2018-03-13 12:44:07 -0400

    Update redis publisher to also use multiple publisher threads

--- lib/active_publisher/async/redis_adapter/consumer.rb
+++ lib/active_publisher/async/redis_adapter/consumer.rb
@@ -9,28 +9,33 @@ module ActivePublisher
           :timeout_interval => 5, # seconds
         }
 
-        attr_reader :consumer, :queue, :supervisor
+        attr_reader :consumers, :queue, :supervisor
 
         def initialize(redis_pool)
           @queue = ::ActivePublisher::Async::RedisAdapter::RedisMultiPopQueue.new(redis_pool, ::ActivePublisher::Async::RedisAdapter::REDIS_LIST_KEY)
-          create_and_supervise_consumer!
+          @consumers = {}
+          create_and_supervise_consumers!
         end
 
-        def create_and_supervise_consumer!
-          @consumer = ::ActivePublisher::Async::InMemoryAdapter::ConsumerThread.new(queue)
+        def create_and_supervise_consumers!
+          ::ActivePublisher.configuration.publisher_threads.times do
+            consumer_id = ::SecureRandom.uuid
+            consumers[consumer_id] = ::ActivePublisher::Async::InMemoryAdapter::ConsumerThread.new(queue)
 
-          supervisor_task = ::Concurrent::TimerTask.new(SUPERVISOR_INTERVAL) do
-            # This may also be the place to start additional publishers when we are getting backed up ... ?
-            unless consumer.alive?
-              consumer.kill rescue nil
-              @consumer = ::ActivePublisher::Async::InMemoryAdapter::ConsumerThread.new(queue)
+            supervisor_task = ::Concurrent::TimerTask.new(SUPERVISOR_INTERVAL) do
+              consumer = consumers[consumer_id]
+              # This may also be the place to start additional publishers when we are getting backed up ... ?
+              unless consumer.alive?
+                consumer.kill rescue nil
+                consumers[consumer_id] = ::ActivePublisher::Async::InMemoryAdapter::ConsumerThread.new(queue)
+              end
+
+              # Notify the current queue size.
+              ::ActiveSupport::Notifications.instrument "redis_async_queue_size.active_publisher", queue.size
             end
 
-            # Notify the current queue size.
-            ::ActiveSupport::Notifications.instrument "redis_async_queue_size.active_publisher", queue.size
+            supervisor_task.execute
           end
-
-          supervisor_task.execute
         end
 
         def size
